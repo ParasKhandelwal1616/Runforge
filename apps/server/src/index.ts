6b0df2d5d71c0e43ec49fb5441e69903e13c6f91
Paras Khandelwal <parasprince161616@gmail.com>
2026-06-11 16:55:01 +0530

    feat: link @runforge/db to apps/server via workspaces

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import connectDB from './config/db.js'
 import { env } from './config/env.js'
+import { Failure  } from '@runforge/db'
+console.log('Models loaded:', { Failure })
 
 const app = express()
 const PORT = env.PORT
